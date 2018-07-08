969ff66c10b8f35e54045d910961782e0be490c2
Bassam Alsanie <bassam.alsanie@motorolasolutions.com>
2018-07-08 03:30:12 -0700

    using statement

---
+++ UsingStatement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UsingStatement
+{
+    class myResource : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("disposing our resources...");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var obj = new myResource())
+            {
+                // Doing stuff with obj
+                Console.WriteLine("using myResource");
+            }
+            // At this poitn the 'Dispose() function will be called on 'obj'
+        }
+    }
+}

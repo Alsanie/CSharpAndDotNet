a096fb400ec474cd27f9f1dc01d56dae1ccf1597
Bassam Alsanie <bassam.alsanie@motorolasolutions.com>
2018-07-08 02:34:30 -0700

    String Interpolation

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new { x = 1, y = 2 };
+
+            // Slow, as it re allocate for every concatenation operator '+'
+            Console.WriteLine("x " + point.x + " y " + point.y);
+
+            // Ok, but in case chnaged the order of the print, you need to chnage the order of the parameters
+            Console.WriteLine("x {0} y {1}", point.x, point.y);
+
+            // Best :)
+            Console.WriteLine($"x {point.x} y {point.y}");
+        }
+    }
+}

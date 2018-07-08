fea650535d5925c060f1b7e398bfbd2d1a9bbdad
Bassam Alsanie <bassam.alsanie@motorolasolutions.com>
2018-07-08 02:28:37 -0700

    Null operations

---
+++ NullCoalescing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullCoalescing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            s = null;
+            var s2 = s ?? "default";  // If s is Null, assign 'default' value instaed
+            Console.WriteLine($"s2 = {s2}");
+
+            // Operator '??' cant be applied to types such as int
+            // To use it with int, you need to use 'int?' instaed of 'int'
+            int? i = 1;
+            i = null;
+            var i2 = i ?? 0;
+            Console.WriteLine($"i2 = {i2}");
+        }
+    }
+}
---
+++ NullCondional/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NullCondional
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = null;
+
+            // Option 1: old way
+            if(person != null)
+                Console.WriteLine(person.Name);
+
+            // Option 2: new way
+            Console.WriteLine(person?.Name);
+
+            // Option 3: combined with null coalescing
+            Console.WriteLine(person?.Name ?? "Name is null");
+        }
+    }
+}
